0ad2a441e1709356aceabb9dc44a9c1b7d125bbf
vitor <vitor.soares@hox.rs>
2023-06-21 17:57:15 -0300

    fix: add body in create user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserBody } from './dtos/create-user-body';
 import { UserRepository } from './repositories/user-repository';
 
 @Controller()
 export class AppController {
   constructor(private userRepository: UserRepository) {}
+  @Get()
+  getHello() {
+    return 'Hello World!';
+  }
 
   @Post('users')
-  async getHello() {
-    await this.userRepository.create('Nome', 'email@gmail.com', '123456');
+  async create(@Body() body: CreateUserBody) {
+    const { email, name, password } = body;
+
+    await this.userRepository.create(name, email, password);
   }
 
   @Get('users')
