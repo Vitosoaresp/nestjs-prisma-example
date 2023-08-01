bc6116b1dbadfbd88c73412ab917e6bcee4aec65
vitor <vitor.soares@hox.rs>
2023-08-01 16:49:02 -0300

    feat: add validation for create user

--- src/user/dtos/create-user-body.ts
+++ src/user/dtos/create-user-body.ts
@@ -1,12 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserBody {
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsNotEmpty()
+  @IsEmail()
   email: string;
 
+  @IsString()
   @IsNotEmpty()
   password: string;
 }
